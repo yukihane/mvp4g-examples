96475f8102e74f46311cb1e5dcd5fafd906e205a
yukihane <yukihane.feather@gmail.com>
2015-11-01 23:54:32 +0900

    remove unused files

--- EmployeeAdminWithGXT/src/main/java/org/gwt4e/mvp4g/example/employeeadminwithgxt/client/Constants.java
+++
@@ -1,8 +0,0 @@
-package org.gwt4e.mvp4g.example.employeeadminwithgxt.client;
-
-public interface Constants {
-
-  public static final String[] DEPARTMENTS = {"Accounting", "Sales", "Plant"};
-  public static final String[] ROLES       = {"Administrator", "Accounts Payable", "Accounts Receivable", "Employee Benefits", "General Ledger",
-                                                 "Payroll", "Inventory", "Production", "Quality Control", "Sales", "Orders", "Customers", "Shipping", "Returns"};
-}
