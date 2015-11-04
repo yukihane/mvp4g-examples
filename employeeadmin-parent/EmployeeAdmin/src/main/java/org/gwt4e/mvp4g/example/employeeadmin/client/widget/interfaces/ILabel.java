a5d6bf18ba5e5a93f746d80cf5a723abc6a539ee
yukihane <yukihane.feather@gmail.com>
2015-11-04 15:13:52 +0900

    delete unused files

--- employeeadmin-parent/EmployeeAdmin/src/main/java/org/gwt4e/mvp4g/example/employeeadmin/client/widget/interfaces/ILabel.java
+++
@@ -1,7 +0,0 @@
-package org.gwt4e.mvp4g.example.employeeadmin.client.widget.interfaces;
-
-public interface ILabel {
-
-  public void setVisible(boolean visible);
-
-}
--- employeeadmin-parent/EmployeeAdmin/src/main/java/org/gwt4e/mvp4g/example/employeeadmin/client/widget/interfaces/IWidget.java
+++
@@ -1,9 +0,0 @@
-package org.gwt4e.mvp4g.example.employeeadmin.client.widget.interfaces;
-
-import com.google.gwt.user.client.ui.Widget;
-
-public interface IWidget {
-
-  public Widget getMyWidget();
-
-}
